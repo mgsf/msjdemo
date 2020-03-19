a9b4e6159a41a8691c9b34e508c63a4c1b58d418
Gayan Fonseka <gayan@Gayans-MacBook-Pro.local>
2020-03-19 09:03:45 +0530

    005-HTTP Delete RequestBody InjectLogger

--- src/main/java/com/gayan/msjdemo/services/BeerServiceImpl.java
+++ src/main/java/com/gayan/msjdemo/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.gayan.msjdemo.services;
 
 import com.gayan.msjdemo.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j   // Inject a logger
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -26,5 +28,10 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         //todo - would add a real impl to update beer
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
